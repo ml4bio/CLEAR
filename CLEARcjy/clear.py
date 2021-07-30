e8dde1115b769da68ad0cc6f70852ab2a9581652
Jiayang Chen <yjcmydkzgj@gmail.com>
2021-07-30 16:04:12 +0800

    update README

--- CLEARcjy/clear.py
+++ CLEARcjy/clear.py
@@ -511,7 +511,7 @@ class ProgressMeter(object):
         entries += [str(meter) for meter in self.meters]
         print('\t'.join(entries))
 
-    def get_batch_fmtstr(self, num_batches):
+    def _get_batch_fmtstr(self, num_batches):
         num_digits = len(str(num_batches // 1))
         fmt = '{:' + str(num_digits) + 'd}'
         return '[' + fmt + '/' + fmt.format(num_batches) + ']'
