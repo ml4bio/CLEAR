4926fdf324f488ad2afb4f63ffef946339153ad2
Jiayang Chen <yjcmydkzgj@gmail.com>
2021-07-31 20:24:16 +0800

    update README

--- clear/pcl/loader.py
+++ clear/pcl/loader.py
@@ -59,7 +59,7 @@ class scRNAMatrixInstance(Dataset):
         self.adata = adata
 
         # data
-        self.data = np.array(self.adata.X)
+        self.data = self.adata.X   #np.array()
 
         # label (if exist, build the label encoder)
         if self.adata.obs.get("x") is not None:
