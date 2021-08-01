c00eecbab2abd837430df83af8e648a4e62aafdb
Jiayang Chen <yjcmydkzgj@gmail.com>
2021-08-01 16:06:48 +0800

    new version

--- README.md
+++ README.md
@@ -23,56 +23,38 @@ conda env create -f environment.yml
 
 ## Running CLEAR
 
-### 1. Prepare Dataset
+### 1. Prepare Data
 
-To help users start their analysis quickly, we provide some python scripts to use. For more individual preprocessing requirements, we also recommend some popular single-cell data analysis tools for data preprocessing instead of using the provided scripts.
+To help users start their analysis quickly, we provide some python scripts to use. 
+For more individual preprocessing requirements, we also recommend some popular single-cell data analysis tools for data preprocessing instead of using the provided scripts.
 
-#### (1). 10X Genomics data
+#### (1). Only Convert Format
+
+For H5AD data,
+```bash
+python preprocess/generate_h5ad.py --input_h5ad_path=Path_to_10X --save_h5ad_dir=Path_to_Save_Folder
+```
+
+For 10X Genomics data
 
 The widely-used 10X platform provides a `10x matrix` data structure: `barcodes.tsv.gz, features.tsv.gz, matrix.tsv.gz`
-
-i. (Recommanded) Use [Scanpy](https://scanpy-tutorials.readthedocs.io/) for data loading and individual data filtering.
-
-```python
-import scanpy as sc
-adata = sc.read_10x_mtx(data_path)
-...(data preprocessing steps with individual paramters, see Scanpy tutorial)
-adata.write('USE_FOR_CLEAR.h5ad')
+```bash
+python preprocess/generate_h5ad.py --input_10X_path=Path_to_10X --save_h5ad_dir=Path_to_Save_Folder
 ```
 
-ii. Use `10X_to_h5ad.py' for quick start. `10X_to_h5ad.py' provides a more uniform data filtering function.
+For CSV data,  If a expression profile (usually a csv file) is provided, 
 
-```python
-
+```bash
+python preprocess/generate_h5ad.py --count_csv_path=Path_to_Count_CSV --label_csv_path=Path_to_Label_CSV --save_h5ad_dir=Path_to_Save_Folder
 ```
 
-#### (2). CSV file
+As for rds files, you can transform them into csv files by the script "rds_to_csv.py" in the "preprocess" folder and then preprocessed them with the same above script.
+The required R environment of this R script need to be set up by yourselves, of which the core package is "SingleCellExperiment"
 
-If a expression profile (usually a csv file) is provided, 
+#### (2). Preprocess if needed
+You can use your own script to accompulish preprocessing and you can also add some input parameters when using the script we offered above. 
 
-i. (Recommanded) Construct `Anndata` and preprocess the data by yourself with more unique filtering parameters
 
-```python
-#create anndata
-df = pd.read_csv('count.csv', index_col=0)
-cellinfo = pd.DataFrame(df.index,index=df.index,columns=['sample_index'])
-geneinfo = pd.DataFrame(df.columns,index=df.columns,columns=['genes_index'])
-adata = sc.AnnData(df, obs=cellinfo, var = geneinfo)
-
-#generate h5ad file for CLEAR input
-...(data preprocessing steps with individual paramters, see Scanpy tutorial)
-adata.write('USE_FOR_CLEAR.h5ad')
-```
-
-ii. Use `csv_to_h5ad.py` for quick start. `csv_to_h5ad.py` provides a more uniform data filtering function. 
-
-```python
-
-```
-
-#### (3). Rdata
-
-For R tool users, we highly recommand you save the filtered csv file and create a h5ad file as input.
 
 ### 2. Apply CLEAR
 
@@ -86,6 +68,24 @@ python CLEAR.py --input_h5ad_path="USE_FOR_CLEAR.h5ad" --obs_label_colname="cell
 ```
 Note: output files are saved in ./result/CLEAR, including embeddings (feature.csv), ground truth labels (gt_label.csv if applicable), cluster results (pd_label.csv if applicable) and some log files (log)
 
+## Example
+Here, we take "deng.rds" dataset for example.
+### 1.download dataset.
+You can either download all of them with the script "download-data.sh" in the "data" folder or use the command in it to download specific dataset.
+Here, we take "deng.rds" dataset for example.
+```
+wget https://ndownloader.figshare.com/files/23872610 -O data/original/h5ad/tmsfpoa-Bladder.h5ad
+```
+
+### 2.generate preprocessed h5ad file.
+```
+python preprocess/generate_h5ad.py --input_h5ad_path="./data/original/h5ad/tmsfpoa-Bladder.h5ad" --save_h5ad_dir="./data/preprocessed/h5ad/" --log --drop_prob=0
+```
+
+### 3.apply CLEAR
+```
+python CLEAR.py --input_h5ad_path="./data/preprocessed/h5ad/tmsfpoa-Bladder_preprocessed.h5ad" --obs_label_colname="cell_ontology_class" --epochs 100 --lr 1 --batch_size 512 --pcl_r 1024 --cos --gpu 0
+```
 
 ## Citation
 
--- preprocess/generate_h5ad.py
+++ preprocess/generate_h5ad.py
@@ -7,9 +7,9 @@ import os
 parser = argparse.ArgumentParser(description='PyTorch scRNA-seq CLR Training')
 
 # input & ouput
-parser.add_argument('--input_h5ad_path', type=str, default="",
+parser.add_argument('--input_h5ad_path', type=str, default=None,
                     help='path to input h5ad files')
-parser.add_argument('--input_10X_path', type=str, default="",
+parser.add_argument('--input_10X_path', type=str, default=None,
                     help='path to input 10X')
 parser.add_argument('--count_csv_path', type=str, default=None,
                     help='path to counts csv')
@@ -17,16 +17,26 @@ parser.add_argument('--label_csv_path', type=str, default=None,
                     help='path to labels csv')
 parser.add_argument('--save_h5ad_dir', type=str, default="./",
                     help='dir to savings')
-parser.add_argument('--label_colname', type=str, default=None,
-                    help='column name of labels in label.csv')
 
 # preprocessing
+parser.add_argument('--filter', action="store_false",
+                    help='Whether do filtering')
+
+parser.add_argument('--norm', action="store_false",
+                    help='Whether do normalization')
+
+parser.add_argument("--log", action="store_false",
+                    help='Whether do log operation')
+
+parser.add_argument("--scale", action="store_false",
+                    help='Whether do log operation')
+
 parser.add_argument('--CPM', action="store_true",
                     help='do count per million operation for raw counts')
-parser.add_argument("--log", action="store_true",
-                    help='Whether do log operation before preprocessing')
+
 parser.add_argument("--highlyGene", action="store_true",
                     help="Whether select highly variable gene")
+
 parser.add_argument("--drop_prob", type=float, default=0.0,
                     help="simulate dropout events")
 
@@ -44,8 +54,7 @@ def dropout_events(adata, drop_prob=0.0):
 
 def preprocess_csv_to_h5ad(
         input_h5ad_path=None, input_10X_path=None, count_csv_path=None, label_csv_path=None, save_h5ad_dir="./",
-        rename_label_colname=None,
-        select_highly_variable_gene=False, do_CPM=True, do_log=True,
+        do_filter=False, do_norm=False, select_highly_variable_gene=False, do_CPM=True, do_log=False, do_scale=False,
         drop_prob=0.0
 ):
     # 1. read data from h5ad, 10X or csv files.
@@ -54,14 +63,14 @@ def preprocess_csv_to_h5ad(
         print("Read data from h5ad file: {}".format(input_h5ad_path))
 
         _, h5ad_file_name = os.path.split(input_h5ad_path)
-        save_file_name = h5ad_file_name.replace(".h5ad", "_preprocessed.h5ad")
+        save_file_name = h5ad_file_name
 
     elif input_10X_path != None and input_h5ad_path == None and count_csv_path == None:
         adata = sc.read_10x_mtx(input_10X_path)
         print("Read data from 10X file: {}".format(input_10X_path))
 
         _, input_10X_file_name = os.path.split(input_10X_path)
-        save_file_name = input_10X_file_name + "_preprocessed.h5ad"
+        save_file_name = input_10X_file_name + ".h5ad"
 
     elif count_csv_path != None and input_h5ad_path == None and input_10X_path == None:
         # read the count matrix from the path
@@ -74,8 +83,8 @@ def preprocess_csv_to_h5ad(
             if count_frame.shape[0] != label_frame.shape[0]:
                 raise Exception("The shapes of counts and labels do not match!")
 
-            if rename_label_colname is not None:
-                label_frame.rename(columns={label_colname: 'x'}, inplace=True)
+            #if rename_label_colname is not None:
+                #label_frame.rename(columns={label_colname: 'x'}, inplace=True)
                 # label_frame.rename(columns={'cell_ontology_class': 'x'}, inplace=True)   # organ celltype
                 # label_frame.rename(columns={'CellType': 'x'}, inplace=True)   # dataset6
                 # label_frame.rename(columns={'celltype': 'x'}, inplace=True)   # dataset1
@@ -91,42 +100,49 @@ def preprocess_csv_to_h5ad(
             print("Read data from csv file: {}".format(count_csv_path))
 
         _, counts_file_name = os.path.split(count_csv_path)
-        save_file_name = counts_file_name.replace(".csv", "_preprocessed.h5ad").replace("_counts", "")
+        save_file_name = counts_file_name.replace(".csv", ".h5ad").replace("_counts", "")
 
     elif input_h5ad_path != None and count_csv_path != None:
         raise Exception("Can not address h5ad and csv files simultaneously!")
 
 
     # 2. preprocess anndata
-    # basic filtering, filter the genes and cells
-    # sc.pp.filter_cells(adata, min_counts=5000)
-    sc.pp.filter_cells(adata, min_genes=200)
+    preprocessed_flag = do_filter | do_norm | select_highly_variable_gene | do_CPM | do_log | do_scale | drop_prob>0
+    # filter operation
+    if do_filter == True:
+        # basic filtering, filter the genes and cells
+        # sc.pp.filter_cells(adata, min_counts=5000)
+        sc.pp.filter_cells(adata, min_genes=200)
 
-    sc.pp.filter_genes(adata, min_cells=3)
+        sc.pp.filter_genes(adata, min_cells=3)
 
-    # calculate the qc metrics, then do the normalization
+        # calculate the qc metrics, then do the normalization
 
-    # filter the mitochondrial genes
-    adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
-    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
-    low_MT_MASK = (adata.obs.pct_counts_mt < 5)
-    adata = adata[low_MT_MASK]
+        # filter the mitochondrial genes
+        adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
+        sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+        low_MT_MASK = (adata.obs.pct_counts_mt < 5)
+        adata = adata[low_MT_MASK]
 
-    # filter the ERCC spike-in RNAs
-    adata.var['ERCC'] = adata.var_names.str.startswith('ERCC-')  # annotate the group of ERCC spike-in as 'ERCC'
-    sc.pp.calculate_qc_metrics(adata, qc_vars=['ERCC'], percent_top=None, log1p=False, inplace=True)
-    low_ERCC_mask = (adata.obs.pct_counts_ERCC < 10)
-    adata = adata[low_ERCC_mask]
+        # filter the ERCC spike-in RNAs
+        adata.var['ERCC'] = adata.var_names.str.startswith('ERCC-')  # annotate the group of ERCC spike-in as 'ERCC'
+        sc.pp.calculate_qc_metrics(adata, qc_vars=['ERCC'], percent_top=None, log1p=False, inplace=True)
+        low_ERCC_mask = (adata.obs.pct_counts_ERCC < 10)
+        adata = adata[low_ERCC_mask]
+
+        preprocessed_flag = True
 
     # dropout operation
-    adata = dropout_events(adata, drop_prob=drop_prob)
+    if drop_prob > 0:
+        adata = dropout_events(adata, drop_prob=drop_prob)
 
     if select_highly_variable_gene and not do_log:
         sc.pp.highly_variable_genes(adata, flavor="seurat_v3", n_top_genes=5000)
         adata = adata[:, adata.var.highly_variable]
 
-    sc.pp.normalize_total(adata, target_sum=1e4, exclude_highly_expressed=True)
-    adata.raw = adata
+    if do_norm == True:
+        sc.pp.normalize_total(adata, target_sum=1e4, exclude_highly_expressed=True)
+        adata.raw = adata
 
     if np.max(adata.X > 100) and do_log:
         sc.pp.log1p(adata)
@@ -143,17 +159,20 @@ def preprocess_csv_to_h5ad(
     # log operations and scale operations will hurt the
     # contrastive between the data
 
-    sc.pp.scale(adata, max_value=10, zero_center=True)
-    # adata[np.isnan(adata.X)] = 0
-    # adata_max = np.max(adata.X)
-    # adata_min = np.min(adata.X)
-    # adata.X = (adata.X - adata_min)/(adata_max - adata_min)
+    if do_scale == True:
+        sc.pp.scale(adata, max_value=10, zero_center=True)
+        # adata[np.isnan(adata.X)] = 0
+        # adata_max = np.max(adata.X)
+        # adata_min = np.min(adata.X)
+        # adata.X = (adata.X - adata_min)/(adata_max - adata_min)
 
     # 3. save preprocessed h5ad
     if save_h5ad_dir is not None:
         if os.path.exists(save_h5ad_dir) != True:
             os.makedirs(save_h5ad_dir)
 
+        if preprocessed_flag == True:
+            save_file_name = save_file_name.replace(".h5ad", "_preprocessed.h5ad")
         save_path = os.path.join(save_h5ad_dir, save_file_name)
 
         adata.write(save_path)
@@ -165,6 +184,7 @@ if __name__=="__main__":
     args = parser.parse_args()
 
     processed_adata = preprocess_csv_to_h5ad(
-        args.input_h5ad_path, args.count_csv_path, args.label_csv_path, args.save_h5ad_dir, args.label_colname,
-        select_highly_variable_gene=args.highlyGene, do_CPM=args.CPM, do_log=args.log, drop_prob=args.drop_prob
+        args.input_h5ad_path, args.input_10X_path, args.count_csv_path, args.label_csv_path, args.save_h5ad_dir,
+        do_filter=args.filter, do_norm=args.norm, select_highly_variable_gene=args.highlyGene,
+        do_CPM=args.CPM, do_log=args.log, drop_prob=args.drop_prob, do_scale=args.scale,
     )
